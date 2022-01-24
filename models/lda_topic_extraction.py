355161fa205cfb0fd8c915c3c48e1d5ee5d2fa16
nolan1999 <arthur.jaques@hispeed.ch>
2022-01-24 17:48:05 +0100

    Working topics extraction with LDA.

--- models/lda_topic_extraction.py
+++ models/lda_topic_extraction.py
@@ -1,5 +1,9 @@
+import json
+import os
+import pandas as pd
 import random
 from sklearn.decomposition import LatentDirichletAllocation
+from featurize import featurize
 
 
 def fit_model(train_data, n_topics, n_docs=None, max_iter=5, random_state=0):
@@ -21,5 +25,21 @@ def get_top_topic_words(lda_model, feature_names, n_words=10):
         top_features_ind = topic.argsort()[: -n_words - 1 : -1]
         top_features = [feature_names[i] for i in top_features_ind]
         weights = topic[top_features_ind]
-        topics.append((weights, top_features))
+        topics.append({n: w for n, w in zip(top_features, weights)})
     return topics
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), '..', 'data', 'preprocessed_data.csv'))
+    count_feats, feature_names = featurize(df, tfidf=True, max_df=0.5, min_df=5)
+    lda_model = fit_model(count_feats, n_topics=6)
+    transformed_data = transform_docs(lda_model, count_feats)
+    topics = get_top_topic_words(lda_model, feature_names)
+    for topic in range(len(topics)):
+        df[f'Topic{topic}'] = transformed_data[:, topic]
+    df.drop(columns='preprocessed_text', inplace=True)
+    df.to_csv(os.path.join(os.path.dirname(__file__), 'transformed_data.csv'))
+    with open(os.path.join(os.path.dirname(__file__), 'topics.json'), 'w') as f:
+        json.dump(topics, f)
+    for topic in topics:
+        print(topic.keys())
